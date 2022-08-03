559e2a6bb6951b5aee71e90dfee2bf2db161cfad
vimalsonara <vvsonara95@gmail.com>
2022-08-03 22:32:48 +0530

    Added buttons to change/reset color

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-const container = document.getElementById('container');
-let rows = document.getElementsByClassName('gridRow');
-let cells = document.getElementsByClassName('cell');
+let color = 'black';
 
-
-function makeGrid(){
-    makeRows(16);
-    makeCols(16);
-};
-
-// run function to make grid
-makeGrid();
-
-// function to make rows
-function makeRows(rowNum) {
-    for (let i = 0; i < rowNum; i++) {
-        let row = document.createElement('div');
-        container.appendChild(row).className = 'gridRow cmn';
+// function make grid
+function makeGrid(size) {
+    let container = document.getElementById('container');
+    let div = container.querySelectorAll('div');
+    div.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;   
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let grid = document.createElement('div');
+        grid.addEventListener('mouseover', colorSquare);
+        grid.style.backgroundColor = 'white';
+        container.appendChild(grid).className = 'grid';
     }
 };
 
-// function to make cell
-function makeCols(cellNum) {
-    for (let i = 0; i < rows.length; i++){
-        for (let j = 0; j < cellNum; j ++) {
-            let newCell = document.createElement('div');
-            rows[j].appendChild(newCell).className = 'cell cmn';
-            newCell.addEventListener('mouseover', randomColor);
-        }
+// function check total grid
+makeGrid(16);
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        makeGrid(input);
+    } else {
+        console.log('Too many grid');
     }
 };
 
-// function to change random color
-function randomColor() {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    let bgColor = "rgb(" + r + "," + g + "," + b + ")";
-    this.style.background = bgColor;
-}
+// function to check color input
+function colorSquare() {
+    if (color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+};
+
+// function change color as per button input
+function changeColor(choice) {
+    color = choice;
+};
+
+// function reset grid color
+function resetGrid() {
+    let container = document.getElementById('container');
+    let div = container.querySelectorAll('div');
+    div.forEach((div) => div.style.backgroundColor = 'white');
+};
